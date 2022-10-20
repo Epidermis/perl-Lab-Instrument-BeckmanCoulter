1c5c20ab7dbd2aad28b00bf30b864cab7e1ca23a
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-10-19 23:17:10 -0500

    Document types of AcT5diff

--- lib/Epidermis/Lab/Instrument/BeckmanCoulter/Hematology/AcT5diff.pm
+++ lib/Epidermis/Lab/Instrument/BeckmanCoulter/Hematology/AcT5diff.pm
@@ -1,7 +1,8 @@
 package Epidermis::Lab::Instrument::BeckmanCoulter::Hematology::AcT5diff;
 # ABSTRACT: Ac•T 5diff Hematology Analyzer
 
-use Moo;
+use strict
+use warnings;
 
 1;
 __END__
@@ -10,6 +11,14 @@ __END__
 
 =head1 SEE ALSO
 
-L<Hematology Analyzer Coulter Ac•T 5diff CP (Cap Pierce)|https://www.beckmancoulter.com/products/hematology/coulter-act-5diff-cp>
+=over 4
+
+=item * Hematology Analyzer Coulter Ac•T 5diff OV (Open Vial)
+
+=item * L<Hematology Analyzer Coulter Ac•T 5diff CP (Cap Pierce)|https://www.beckmancoulter.com/products/hematology/coulter-act-5diff-cp>
+
+=item * L<Hematology Analyzer Coulter Ac•T 5diff AL (Autoloader)|https://www.beckmancoulter.com/products/hematology/coulter-act-5diff-al>
+
+=back
 
 =cut
